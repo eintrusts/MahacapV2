ba3da2433f1f486fcbc11d609d2f5422f3268237
EinTrust <eintrusts@gmail.com>
2025-09-27 09:02:33 +0530

    Update mahacap.py

--- mahacap.py
+++ mahacap.py
@@ -710,35 +710,3 @@ elif st.session_state.current_page == "Admin":
     admin_panel()
 else:
     st.title("Welcome to Maharashtra CAP Dashboard")
-
-#---------------------Google Drive Folder and File Upload Functions----------------------------
-from googleapiclient.http import MediaFileUpload
-from gdrive_auth import get_drive_service
-
-def get_or_create_folder(city_name):
-    service = get_drive_service()
-    # Check if folder exists
-    results = service.files().list(
-        q=f"name='{city_name}' and mimeType='application/vnd.google-apps.folder'",
-        spaces='drive'
-    ).execute()
-    items = results.get('files', [])
-    if items:
-        return items[0]['id']
-    # Create folder
-    file_metadata = {
-        'name': city_name,
-        'mimeType': 'application/vnd.google-apps.folder'
-    }
-    folder = service.files().create(body=file_metadata, fields='id').execute()
-    return folder.get('id')
-
-def upload_file_to_folder(file_path, folder_id):
-    service = get_drive_service()
-    file_metadata = {
-        'name': os.path.basename(file_path),
-        'parents': [folder_id]
-    }
-    media = MediaFileUpload(file_path, resumable=True)
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    return file.get('id')
