7cb8d71e2b5622ae0d9fc551bb6b1932ff1df9cc
EinTrust <eintrusts@gmail.com>
2025-09-29 16:44:04 +0530

    Update gdrive_auth.py

--- gdrive_auth.py
+++ gdrive_auth.py
@@ -1,7 +1,7 @@
+import os
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import os
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
@@ -14,7 +14,11 @@ def get_drive_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            # Use run_console() for headless/cloud; run_local_server() for local dev
+            if os.environ.get('STREAMLIT_SERVER_HEADLESS', '0') == '1' or os.environ.get('IS_STREAMLIT_CLOUD'):
+                creds = flow.run_console()
+            else:
+                creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return build('drive', 'v3', credentials=creds)
